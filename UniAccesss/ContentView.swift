348fc33a4875a9e2193732df22a64e4528d6f109
SinckCode <sinckinfo@gmail.com>
2026-06-02 15:40:18 -0600

    Initial Commit

---
+++ UniAccesss/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UniAccesss
+//
+//  Created by Onesto on 02/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UniAccesss/UniAccesssApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniAccesssApp.swift
+//  UniAccesss
+//
+//  Created by Onesto on 02/06/26.
+//
+
+import SwiftUI
+
+@main
+struct UniAccesssApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
